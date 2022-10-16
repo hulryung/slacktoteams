ac2bef8fb7a20e5c18410513621fd21fb7747f27
Daekeun Kang <dkkang@huconn.com>
2022-10-16 14:52:21 +0000

    Replaced module for Slack with slack_bolt

--- app/bot.py
+++ app/bot.py
@@ -2,12 +2,10 @@
 FLEX.team service is not supported by the python library, so we need to
 make a bot to forward Slack message to MS Teams channel.
 '''
-import slack
 import os
+from slack_bolt import App
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask, request, Response
-from slackeventsapi import SlackEventAdapter
 from pymsteams import connectorcard
 import emoji_data_python
 
@@ -19,34 +17,51 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 port = int(os.environ.get('FLEXBOT_PORT', 5080))
-hostname = os.environ.get('HOSTNAME', '0.0.0.0')
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
-client = slack.web.client.WebClient(token=os.environ['SLACK_TOKEN'])
+token = os.environ.get('SLACK_BOT_TOKEN')
+signing_secret = os.environ.get('SLACK_SIGNING_SECRET')
+app = App(token=token, signing_secret=signing_secret)
 
-client.chat_postMessage(channel='#'+os.environ['SLACK_CHANNEL'], text="FLEX Bot has started!")
-BOT_ID = client.api_call("auth.test")['user_id']
+@app.message("hello")
+def say_hello(message, say):
+    user = message['user']
+    say(f"Hi there, <@{user}>!")
+    print(message)
 
-# teams webhook
-teamsMessage = connectorcard(os.environ['TEAMS_WEBHOOK_URL'])
+@app.message("휴가")
+def say_vacation(message, say):
+    text = convert_emoji(message['text'])
+    teamsMessage.text(text)
+    teamsMessage.send()
+    print(message)
 
-# slack event
-@slack_event_adapter.on('message')
-def message(payload):
-    print(payload)
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    if BOT_ID != user_id:
-        if '휴가' in text:
-            # convert colon emoji to unicode
-            text = convert_emoji(text)
-            teamsMessage.text(text)
-            # send message to teams channel
-            teamsMessage.send()
-        else:
-            print(text)
+@app.message("Calendar")
+def say_calendar(message, say):
+    print("Calendar")
+    print(message)
+    text = convert_emoji(message['text'])
+    teamsMessage.text(text)
+    teamsMessage.send()
+    print(message)
+
+
+@app.event("message")
+def message_event_handler(event, say):
+    print("message_event_handler")
+    attachment = event['attachments'][0]
+    # text = convert_emoji(attachment['text'])
+    pretext = convert_emoji(attachment['pretext'])
+    title = convert_emoji(attachment['title'])
+    #fallback = convert_emoji(attachment['fallback'])
+    teamsMessage.text(pretext)
+    # markdown to plain text
+    teamsMessage.title(title)
+    teamsMessage.send()
+    print(event)
 
 if __name__ == "__main__":
-    app.run(debug=True, host=hostname, port=port)
+    print("FLEX Bot has started!")
+    # teams webhook
+    teamsMessage = connectorcard(os.environ['TEAMS_WEBHOOK_URL'])
+
+    app.start(port=port)
+    
